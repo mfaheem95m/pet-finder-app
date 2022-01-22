259ca8171b1d133e814f5609239093f4b7c57f33
Muhammad Faheem <mfaheem95m@gmail.com>
2022-01-22 15:56:44 +0500

    create a form in searchparams component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useState } from "react"
+const ANIMALS =  ["bird", "cat", "dog", "rabbit", "reptile"];
+const breeds =[]
  const SearchParams = () => {
    const [location, updateLocation] =useState("Seattle, WA");
+   const [animal,updateAnimal] = useState("")
+  const [breed,updateBreed] = useState("")
+
   return (
     <div className="search-params">
       <form>
@@ -12,9 +17,49 @@ import { useState } from "react"
          value = {location}
          placeholder = "Location"
          onChange = {(e) => updateLocation(e.target.value)}
+
          />
 
        </label>
+       <label htmlFor = "animal">
+         Animal
+         <select
+         id = "animal"
+         value = {animal}
+         onChange = {(e) => updateAnimal(e.target.value)}
+         onBlur = {(e) => updateAnimal(e.target.value)}
+         >
+          <option/>
+          {
+            ANIMALS.map((animal) => (
+           <option value={animal} key={animal}>
+             {animal}
+           </option>
+            ))
+          }
+       </select>
+       </label>
+
+       <label htmlFor = "breed">
+        Breed
+         <select
+         disabled = {!breeds.length}
+         id = "animal"
+         value = {breed}
+         onChange = {(e) => updateBreed(e.target.value)}
+         onBlur = {(e) => updateBreed(e.target.value)}
+         >
+          <option/>
+          {
+            breeds.map((breed) => (
+           <option value={breed} key={breed}>
+             {breed}
+           </option>
+            ))
+          }
+       </select>
+       </label>
+
        <button>Submit</button>
       </form>
 
