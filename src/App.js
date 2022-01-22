ba55593864759d0ba9546bd341f0dfbe90af59bd
Muhammad Faheem <mfaheem95m@gmail.com>
2022-01-22 13:53:10 +0500

    create searchparams component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
 import "./App.css"
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1>Adopt me</h1>
-      
+      <SearchParams/>
     </div>
   )
 };
