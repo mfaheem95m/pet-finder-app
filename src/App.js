d8bb6c584c32f7002edae99abcb35f93de5d8743
Muhammad Faheem <mfaheem95m@gmail.com>
2022-01-23 04:10:10 +0500

    create details component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react"
 import "./App.css"
+import {BrowserRouter as Router,Route,Switch,Link} from "react-router-dom"
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt me</h1>
-      <SearchParams/>
+      <Router>
+        <header>
+           <Link to = "/">
+        Adopt me!
+        </Link>
+        </header>
+
+
+        <Switch>
+            <Route path = "/details/:id">
+          <Details/>
+        </Route>
+        <Route path= "/">
+       <SearchParams/>
+        </Route>
+        </Switch>
+
+      </Router>
+
     </div>
   )
 };
